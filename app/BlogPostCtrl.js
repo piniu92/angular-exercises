66414e00f527bfc090edf6e9617c56061a527294
piniu <rafal.pinas@gmail.com>
2014-09-01 12:20:39 +0200

    ex 1

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,7 +1,11 @@
 (function () {
     'use strict';
     function BlogPostCtrl() {
-
+        this.posts = [
+            {ID: 0, Name: "Ala", Pets: "cat"},
+            {ID: 1, Name: "Marysia", Pets: "ryś"},
+            {ID: 2, Name: "Jan", Pets: "lala"}
+        ];
     }
 
     var module = angular.module("exerciseApp", []);

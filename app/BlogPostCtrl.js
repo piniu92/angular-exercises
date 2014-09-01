e686f6f1ed24f9317380ab8ca55f9ba50aef539e
piniu <rafal.pinas@gmail.com>
2014-09-01 13:37:52 +0200

    ex 2

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,7 +1,10 @@
 (function () {
     'use strict';
     function BlogPostCtrl(PostDAO) {
-
+        var ctrl = this;
+        PostDAO.query().then(function (data) {
+            ctrl.posts = data;
+        });
     }
 
     var module = angular.module("exerciseApp");

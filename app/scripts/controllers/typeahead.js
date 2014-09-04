572a37fbb3f9b9121d9417a9e3e4303414aad37c
piniu <rafal.pinas@gmail.com>
2014-09-04 09:58:01 +0200

    ex 7

--- app/scripts/controllers/typeahead.js
+++ app/scripts/controllers/typeahead.js
@@ -3,12 +3,21 @@
 angular.module('itcApp').controller('TypeaheadCtrl', function ($filter,ContactDAO) {
 
     this.selected = undefined;
+    var ctrl = this;
 
-    this.getContacts=function(typedValue){
+    this.getContacts = function (typedValue) {
 	// need to write this function body
+        ContactDAO.getAll().then(function (data) {
+            ctrl.list = data[0];
+        });
+
+
     }
 
     this.selectContact=function(item,model,label){
+
         this.selected=item;
     }
+
+
 });

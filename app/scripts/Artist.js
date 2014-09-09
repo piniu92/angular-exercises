106adedcb9221102f9b12d1eb6ec1d1b3a1a773e
piniu <rafal.pinas@gmail.com>
2014-09-09 09:48:37 +0200

    ex 10

--- app/scripts/Artist.js
+++ app/scripts/Artist.js
@@ -5,6 +5,16 @@
  */
 angular.module('ngselect2App').factory('Artist',['$resource',function($resource){
     var ws_url = 'http://musicbrainz.org/ws/2/artist';
-    var artist = $resource(ws_url,{fmt:'json',limit:10},{});
-    return artist;
+    var artist = $resource(ws_url, {fmt: 'json', limit: 10}, {
+        query: {isArray: false}
+    });
+    return {
+        query: function (searchString) {
+            if (!searchString) {
+                throw new Error('SearchString must not be empty');
+            }
+            return artist.query({query: searchString}).$promise;
+        }
+    }
+
 }]);
--- app/scripts/controllers/ngselect2.js
+++ app/scripts/controllers/ngselect2.js
@@ -8,15 +8,33 @@
  * Controller of the ngselect2App
  */
 angular.module('ngselect2App').controller('Ngselect2Ctrl', function ($scope, $resource, Artist) {
-
+    var ctrl = this;
     this.artists = [];
     this.artistModel = [];
 
-    this.search=function () {
-        // recive argument/s and code function body
-    }
+    this.search = function (queryObj) {
+
+        Artist.query(queryObj.term).then(function (results) {
+            ctrl.artists = results.artist.map(function (artist) {
+                artist['text'] = artist.name;
+                return artist;
+            });
+            //ctrl.artist = results.artist;
+            var data = { results: ctrl.artists };
+            console.log(ctrl.artist);
+            queryObj.callback(data);
+        });
+
+    };
+
 
     // fill with 4 items below
-    this.select2options = {}
-
+    this.select2options = {
+        'multiple': true,
+        'query': function (query) {
+            ctrl.search(query)
+        },
+        minimumInputLength: 1,
+        maximumInputLength: 10
+    };
 });
